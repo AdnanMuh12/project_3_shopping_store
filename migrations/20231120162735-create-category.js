b24ff22cbf02e2766906112728d017d3c5baa963
AdnanMuh12 <adnansmk1@gmail.com>
2023-11-23 10:06:47 +0800

    main

--- migrations/20231120162735-create-category.js
+++ migrations/20231120162735-create-category.js
@@ -10,7 +10,8 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       type: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        unique: true
       },
       sold_product_amount: {
         type: Sequelize.INTEGER
